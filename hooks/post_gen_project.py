2ff67fe83cfd8dae594611ba0de9e15554427963
Carlos Lugones <contact@carloslugones.com>
2021-08-08 16:41:56 -0400

    fix: pos_gen_project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,12 +11,7 @@ def remove_file(filepath):
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
-        remove_file('AUTHORS.rst')
-        remove_file('docs/authors.rst')
-
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
+        remove_file('AUTHORS.md')
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
